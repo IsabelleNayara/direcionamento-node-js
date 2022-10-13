9c1cac91a0e0b3c93bcde39e22a7ff2b4a4c1102
Isabelle Duarte <aecisabelle@gmail.com>
2022-10-13 19:47:10 -0300

    alguns detalhes de divisão de listas

--- desafio4.js
+++ desafio4.js
@@ -10,3 +10,4 @@ console.log("A sala 1 tem " + sala1.length + " alunos. Segue os nomes abaixo:");
 console.log(`Alunos da sala1 ${sala1}`);
 console.log("A sala 2 tem " + sala2.length + " alunos. Segue os nomes abaixo:");
 console.log(`Alunos da sala2 ${sala2}`)
+
