2fcb80524c1cd7c57a83692cca68ca5a48eb2bb4
Isabelle Duarte <aecisabelle@gmail.com>
2022-10-13 20:33:02 -0300

    Concatenando arrays com .concat

---
+++ desafio6.js
@@ -0,0 +1,7 @@
+// desafio juntando duas salas. Haverá uma palestra sobre padrões de projetos para as alunas das salas de JavaScript e Python. Junte ambas as salas em uma única lista que exiba todas as alunas. 
+const salaPython = ["Melissa", "Helena", "Rodrigo"];
+const salaJavaScript = ["Ju", "Leo", "Raquel"];
+
+const salasUnificadas = salaPython.concat(salaJavaScript);
+console.log(salasUnificadas)
+
