8bdb1d6f599d0d4f9ebe1676fea1110b2d582277
Isabelle Duarte <aecisabelle@gmail.com>
2022-10-14 22:10:41 -0300

    desafio for

---
+++ desafio9.js
@@ -0,0 +1,12 @@
+// Desafio for
+
+// Imprima o índice e a lista com os seguintes numeros:
+
+// 100, 200, 300, 400, 500, 600
+
+const numeros = [100, 200, 300, 400, 500, 600];
+for (let i = 0; i < numeros.length; i++){
+    console.log(i, numeros[i]);
+}
+
+//for é um laço de repetição (looping)
