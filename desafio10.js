48ab8e75245758b220c726cc62f4ce03bcb7df11
Isabelle Duarte <aecisabelle@gmail.com>
2022-10-17 21:20:20 -0300

    média com for

---
+++ desafio10.js
@@ -0,0 +1,21 @@
+// Desafio média com for
+
+// Calcular a média entre as seguintes notas usando o for:
+
+// 10, 6.5, 8 e 7.5;
+
+const notas = [10, 6.5, 8, 7.5];
+let somaDasNotas = 0;
+
+for (let i = 0; i < notas.length; i++){
+    somaDasNotas += notas[i];
+}
+
+let media = somaDasNotas/notas.length
+
+console.log(media)
+
+
+
+
+
